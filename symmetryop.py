93ddd58a0ee472e66553501da9cbc231002cbf2c
yefengjh <yefengcn@gmail.com>
2018-05-23 19:38:42 -0400

    new example

--- symmetryop.py
+++ symmetryop.py
@@ -11,3 +11,19 @@ print "Operations:"
 
 for op in symOps:
         print op.getIdentifier()
+
+symOp = SymmetryOperationFactory.createSymOp("x-y,x,z")
+
+coordinates = [0.3, 0.4, 0.5]
+coordinatesPrime = symOp.transformCoordinates(coordinates)
+
+print "Transformed coordinates:", coordinatesPrime
+
+
+symOps = SpaceGroupFactory.subscribedSpaceGroupSymbols(206).getSymmetryOperations()
+
+print "Number of operations:", len(symOps)
+print "Operations:"
+
+for op in symOps:
+        print op.getIdentifier()
