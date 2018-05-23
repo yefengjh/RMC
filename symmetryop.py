3af84e1d725edb9d3910fb9d20cf81fb39ea795d
yefengjh <yefengcn@gmail.com>
2018-05-23 18:46:30 -0400

    add example

--- symmetryop.py
+++ symmetryop.py
@@ -3,6 +3,11 @@ import numpy as np
 from mantid.simpleapi import *
 from mantid.geometry import SpaceGroupFactory, SymmetryOperationFactory
 
-symOp = SymmetryOperationFactory.createSymOp("x,y,-z")
+# symOp = SymmetryOperationFactory.createSymOp("x,y,-z")
 
 symOps = SymmetryOperationFactory.createSymOps("x,y,-z; -x,-y,-z; z,x,y")
+print "Number of operations:", len(symOps)
+print "Operations:"
+
+for op in symOps:
+        print op.getIdentifier()
