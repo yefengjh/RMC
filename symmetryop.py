f9519b03a0efb9488b93ecf6ec65292f237a4c7a
yefengjh <yefengcn@gmail.com>
2018-05-23 23:17:27 -0400

    find unique coordinates

--- symmetryop.py
+++ symmetryop.py
@@ -4,25 +4,17 @@ from mantid.simpleapi import *
 from mantid.geometry import SpaceGroupFactory, SymmetryOperationFactory
 
 # symOp = SymmetryOperationFactory.createSymOp("x,y,-z")
+#symOps = SymmetryOperationFactory.createSymOps("x,y,-z; -x,-y,-z; z,x,y")
+#print "Number of operations:", len(symOps)
+#print "Operations:"
+#for op in symOps:
+#        print op.getIdentifier()
 
-symOps = SymmetryOperationFactory.createSymOps("x,y,-z; -x,-y,-z; z,x,y")
-print "Number of operations:", len(symOps)
-print "Operations:"
-
-for op in symOps:
-        print op.getIdentifier()
-
-symOp = SymmetryOperationFactory.createSymOp("x-y,x,z")
-
-coordinates = [0.3, 0.4, 0.5]
-coordinatesPrime = symOp.transformCoordinates(coordinates)
-
-print "Transformed coordinates:", coordinatesPrime
 
 #  three different symmetry operation expressions
 #symOps='206'
-#symOps='I a -3'
-symOps='x,y,z; -y,x-y,z+1/3; -x+y,-x,z+2/3; y,x,-z; x-y,-y,-z+2/3; -x,-x+y,-z+1/3'
+#symOps='x,y,z; -y,x-y,z+1/3; -x+y,-x,z+2/3; y,x,-z; x-y,-y,-z+2/3; -x,-x+y,-z+1/3'
+symOps='I a -3'
 
 try:
     symOps = SpaceGroupFactory.subscribedSpaceGroupSymbols(int(symOps))[0]
@@ -32,8 +24,20 @@ if SpaceGroupFactory.isSubscribedSymbol(symOps):
     symOps = SpaceGroupFactory.createSpaceGroup(symOps).getSymmetryOperations()
 else:
     symOps = SymmetryOperationFactory.createSymOps(symOps)
-print "Number of operations:", len(symOps)
-print "Operations:"
 
+#print "Number of operations:", len(symOps)
+#print "Operations:"
+#for op in symOps:
+#    print op.getIdentifier()
+
+totcoordinate=[]
+coordinates = [0, 0, 0]
 for op in symOps:
-        print op.getIdentifier()
+    coordinatesPrime = op.transformCoordinates(coordinates)
+    print('{:<20s},{:10s}').format(op.getIdentifier(),coordinatesPrime)
+    if coordinatesPrime not in totcoordinate:
+        totcoordinate.append(coordinatesPrime)
+        
+unique_coordinate=np.unique(totcoordinate,axis=0)
+print unique_coordinate
+
